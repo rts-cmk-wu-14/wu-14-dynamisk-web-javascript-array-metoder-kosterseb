adf4de1dd86fef5e11535b7146060f599b538b37
kosterseb <sebkos.code@gmail.com>
2025-08-25 08:58:11 +0200

    opgave 3b løst

--- arrays.js
+++ arrays.js
@@ -26,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check om der i dogsArray er en hund som hedder "Baxter".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3b", ...)
+    console.log("opgave 3b:", dogsArray.includes("Baxter"));
     
 
     //     Opgave 4a
